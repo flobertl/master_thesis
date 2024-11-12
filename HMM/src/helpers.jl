fbdcd748b5f68ae6c89d8bd01ff690a747eb31ae
Flo Schimek <florian.schimek@gmx.at>
2024-11-13 00:40:29 +0100

    Add Helper Fun backwardCalc. #7

--- HMM/src/helpers.jl
+++ HMM/src/helpers.jl
@@ -26,4 +26,8 @@ function forwardCalc(alpha_tminus1::Array{Float64, 1}, a_point_i::Array{Float64,
     alpha_tminus1 .* a_point_i .* b_point_o_t
 end
 
+function backwardCalc(beta_tplus1::Array{Float64, 1}, a_i_point::Array{Float64, 1}, b_point_o_tplus1::Array{Float64, 1})
+    beta_tplus1 .* a_i_point .* b_point_o_tplus1
+end
+
 end
