8e3a8bcd3321c538b708df96dc5e2bb043396642
Flo Schimek <florian.schimek@gmx.at>
2024-11-12 21:23:10 +0100

    First test/Testing tests. #2

--- HMM/src/test.jl
+++ HMM/src/test.jl
@@ -14,6 +14,19 @@ function testingEquality(testName::String, testingValue, expectedResult)
     end
 end
 
+module ForwardAlgo
+    function testForwardCalc()
+        name = "Helper forwardCalc"
+
+        alpha_tminus1 = ones(4)
+        a_point_i = ones(4)
+        b_point_o_t = ones(4)
+        value = forwardCalc(alpha_tminus1, a_point_i, b_point_o_t)
+
+        testingEquality(value, 4)
+    end
+end
+
 function testAll()
     ()
 end
