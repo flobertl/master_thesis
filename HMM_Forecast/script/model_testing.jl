4eaeb4941ad8bfe9af71a7ea528f8628ec78e082
bumbledor <antoniojardim@hotmail.co.uk>
2025-02-02 14:49:33 +0100

    Fail run 250states. #27

---
+++ HMM_Forecast/script/model_testing.jl
@@ -0,0 +1,78 @@
+# Function to run BasisModel runBasisModelAnalysis
+using(Pkg)
+Pkg.activate("HMM_Forecast")
+using Revise, Dates, Statistics, Random, Plots
+using HMM_Forecast
+
+# Set Parameters
+hh = 1
+states = [1]
+Random.seed!(42) 
+iter = 10
+
+Random.seed!(42)
+
+# Data
+(observationSpace, observations, observationsAsIndeces) = HMM_Forecast.getData2Years(hh);
+dates = HMM_Forecast.dateTimesOf2YearsData()
+dateIndeces = HMM_Forecast.calcFirstQHofYearAndMonth()
+
+startIndexTraining  = dateIndeces[2,1] 
+endIndexTraining    = dateIndeces[3,1]-1
+startIndexTest      = dateIndeces[3,1]
+endIndexTest        = HMM_Forecast.endOfDecember20()
+indecesSpring20 = dateIndeces[3,3]:dateIndeces[3,6]-1
+indecesSummer20 = dateIndeces[3,6]:dateIndeces[3,9]-1
+indecesFall20   = dateIndeces[3,9]:dateIndeces[3,12]-1
+indecesWinter20 = vcat(dateIndeces[3,1]:dateIndeces[3,3]-1, dateIndeces[3,12]:endIndexTest)
+
+dataTrainingAsIndeces   = observationsAsIndeces[startIndexTraining:endIndexTraining]
+dataTestAsIndeces       = observationsAsIndeces[startIndexTest:endIndexTest]
+dataTraining            = observations[startIndexTraining:endIndexTraining]
+dataTests               = observations[startIndexTest:endIndexTest]
+
+prevTime = now()
+carme = Dict()
+
+hmm = HMM_Forecast.runBWAlgoWithRandomInit((observationSpace, dataTrainingAsIndeces), 1, 1);
+
+for N in states
+    # Train and store model 
+    println("\n ########################### MODEL $N states #################################")
+    println("-------------- Train Model with $N states------------------")
+    hmm, logliklihood_hmm = HMM_Forecast.runBWAlgoWithRandomInit((observationSpace, dataTrainingAsIndeces), N, iter);
+    HMM_Forecast.saveHMM(hmm, "basismodel_hh($hh)//states($N)//basismodel_states($N)")
+    # N = 50
+    # hmm1 = loadHMM("basismodel_hh(1)//states(1)//basismodel_states(1)")
+    prevTime = HMM_Forecast.printTimeAndResetTimeStamp(prevTime)
+    
+    # Calc Prediction
+    println("-------------- Calc forecast distribution for {$N} states------------------")
+    distributionForecastVector = HMM_Forecast.createSeveralOneStepPredictions(hmm, dataTrainingAsIndeces, dataTestAsIndeces)::Vector{Vector{Float32}}
+    prevTime = HMM_Forecast.printTimeAndResetTimeStamp(prevTime)
+
+    # plotDistributionForecastWithViolin(hmm, dataTraining[end-20:end], dataTests[3301:3320], distributionForecastVector[1:20])
+
+    # Generate PIT Plots
+    println("-------------- Generate PT plots for {$N} states------------------")
+    pitYear = HMM_Forecast.plotPIT(hmm, dataTests, distributionForecastVector, "PIT of Year 2020")
+    pitSpring = HMM_Forecast.plotPIT(hmm, observations[indecesSpring20], distributionForecastVector[indecesSpring20 .- (startIndexTest-1)], "Spring")
+    pitSummer = HMM_Forecast.plotPIT(hmm, observations[indecesSummer20], distributionForecastVector[indecesSummer20 .- (startIndexTest-1)], "Summer")
+    pitFall = HMM_Forecast.plotPIT(hmm, observations[indecesFall20], distributionForecastVector[indecesFall20 .- (startIndexTest-1)], "Fall")
+    pitWinter = HMM_Forecast.plotPIT(hmm, observations[indecesWinter20], distributionForecastVector[indecesWinter20 .- (startIndexTest-1)], "Winter")
+    pitSeasons = plot(pitSpring, pitSummer, pitFall, pitWinter, layout=(2,2))
+    pit0300 = HMM_Forecast.plotPIT(hmm, dataTests[1+3*4:96:end], distributionForecastVector[1+3*4:96:end], "03:00")
+    pit0900 = HMM_Forecast.plotPIT(hmm, dataTests[1+9*4:96:end], distributionForecastVector[1+9*4:96:end], "09:00")
+    pit01500 = HMM_Forecast.plotPIT(hmm, dataTests[1+15*4:96:end], distributionForecastVector[1+15*4:96:end], "15:00")
+    pit2100 = HMM_Forecast.plotPIT(hmm, dataTests[1+21*4:96:end], distributionForecastVector[1+21*4:96:end], "21:00")
+    pitSpecificTimes = plot(pit0300, pit0900, pit01500, pit2100, layout=(2,2))
+    png(pitYear, ".//HMM_Forecast//tmp//basismodel_hh($hh)//states($N)//pitYear.png")
+    png(pitSpecificTimes, ".//HMM_Forecast//tmp//basismodel_hh($hh)//states($N)//pitSpecificTimes.png")
+    png(pitSeasons, ".//HMM_Forecast//tmp//basismodel_hh($hh)//states($N)//pitSeasons.png")
+    prevTime = HMM_Forecast.printTimeAndResetTimeStamp(prevTime)
+
+    # Calc average mean error
+    push!(carme, N => HMM_Forecast.calAverageRelativeMeanError(observationSpace, dataTests, distributionForecastVector))
+end
+
+println(carme)
