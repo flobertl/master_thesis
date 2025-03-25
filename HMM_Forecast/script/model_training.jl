0acc4b886910a10915c7a2060116a8941a7b92b7
Flo Schimek <florian.schimek@gmx.at>
2025-03-25 09:08:34 +0100

    Train basismodel hh=1 states=80

--- HMM_Forecast/script/model_training.jl
+++ HMM_Forecast/script/model_training.jl
@@ -5,8 +5,8 @@ using Revise, Dates, Statistics, Random, Plots
 using HMM_Forecast
 
 # Parameters
-hhVector = [1]
-numberOfStatesVector = [150] #vcat(5:5:30,40:10:60)
+hh = 1
+numberOfStatesVector = [80] #vcat(5:5:30,40:10:60)
 numberOfTimeBlocks = [4, 8, 12, 24, 96]
 
-HMM_Forecast.trainAllModels(hhVector, numberOfStatesVector, numberOfTimeBlocks)
+HMM_Forecast.trainBasisModels(hh, numberOfStatesVector)
