52dc4ff2f9dd0852017c9363111847873c67376b
Flo Schimek <florian.schimek@gmx.at>
2025-03-27 08:30:31 +0100

    Train Models. #31

--- HMM_Forecast/script/model_testing.jl
+++ HMM_Forecast/script/model_testing.jl
@@ -6,10 +6,12 @@ using HMM_Forecast
 
 # Set Parameters
 hhs = 1
-numberOfStatesVector =  [10, 20, 40, 60, 80] #vcat(5:5:30,40:10:60)
-historicWindowLengthVector = [5, 10, 50, 100]
+numberOfStatesVector =  [10, 20, 30, 40, 50, 60, 80, 100] #vcat(5:5:30,40:10:60)
+historicWindowLengthVector = [5, 10, 20, 50]
 
 
 #results = HMM_Forecast.evaluateSeasonmodels(numberOfStatesVector, hh)
 #results = HMM_Forecast.evaluateSeasonmodelsWithTimeStamps(numberOfTimeBlocks, numberOfStatesVector, hh)
 HMM_Forecast.basismodelHyperparameterAnalysis(hhs, numberOfStatesVector, historicWindowLengthVector)
+
+#results = HMM_Forecast.evaluateBasismodel(1, [80, 100])
--- HMM_Forecast/script/model_training.jl
+++ HMM_Forecast/script/model_training.jl
@@ -5,8 +5,9 @@ using Revise, Dates, Statistics, Random, Plots
 using HMM_Forecast
 
 # Parameters
-hh = 1
-numberOfStatesVector = [80] #vcat(5:5:30,40:10:60)
+hh = 2
+numberOfStatesVector = [80, 100] #vcat(5:5:30,40:10:60)
 numberOfTimeBlocks = [4, 8, 12, 24, 96]
 
-HMM_Forecast.trainBasisModels(hh, numberOfStatesVector)
+HMM_Forecast.trainBasisModels(2, numberOfStatesVector)
+HMM_Forecast.trainBasisModels(3, numberOfStatesVector)
