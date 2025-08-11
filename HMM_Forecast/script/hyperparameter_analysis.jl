970489041325bd46365ba6f01e4dcdc6923238cf
Flo Schimek <florian.schimek@gmx.at>
2025-08-11 10:10:09 +0200

    Training models hh 4+5. #38

--- HMM_Forecast/script/hyperparameter_analysis.jl
+++ HMM_Forecast/script/hyperparameter_analysis.jl
@@ -4,8 +4,8 @@ Pkg.activate("HMM_Forecast")
 using Revise, Dates, Statistics, Random, Plots
 using HMM_Forecast
 
-# Parameters
-hhs = [2, 3] # 4, 5]
+# Parameterslk
+hhs = [ 4, 5]
 numberOfObservationsVector =  [25, 50, 100, 200]
 numberOfStatesVector = [10, 20, 30, 40, 50, 60, 70, 80]
 
