784a4fca398404dc96c07d98a8c54a811a84d069
Flo Schimek <florian.schimek@gmx.at>
2025-08-18 12:56:48 +0200

    Tidy up.

--- HMM_Forecast/script/hyperparameter_analysis.jl
+++ HMM_Forecast/script/hyperparameter_analysis.jl
@@ -6,19 +6,22 @@ using HMM_Forecast
 
 # Parameters
 hhs = [1, 2, 3, 4, 5]
-numberOfObservationsVector =  [25, 50, 100, 200]
-numberOfStatesVector = [10, 20, 30, 40, 50, 60, 70, 80, 100]
+numberOfObservationsVector =  [100, 200]
+numberOfStatesVector = [120] #[10, 20, 30, 40, 50, 60, 70, 80]
 
 for hh in hhs
     # Run Training
-    # HMM_Forecast.trainBasisModel(hh, "A", numberOfObservationsVector, numberOfStatesVector)
-    # HMM_Forecast.trainBasisModel(hh, "B", numberOfObservationsVector, numberOfStatesVector)
+    HMM_Forecast.trainBasisModel(hh, "A", numberOfObservationsVector, numberOfStatesVector)
+    HMM_Forecast.trainBasisModel(hh, "B", numberOfObservationsVector, numberOfStatesVector)
 
     # Run evaluation
-/
+    HMM_Forecast.hyperparameterAnalysis(hh, "A", numberOfObservationsVector, numberOfStatesVector)
+    HMM_Forecast.hyperparameterAnalysis(hh, "B", numberOfObservationsVector, numberOfStatesVector)
+
 
     # # Plot results
-    HMM_Forecast.plotHyperparameterAnalysis(hh, "A")
-    HMM_Forecast.plotHyperparameterAnalysis(hh, "B")
+    # HMM_Forecast.plotHyperparameterAnalysis(hh, "A")
+    # HMM_Forecast.plotHyperparameterAnalysis(hh, "B")
 end
+
 
