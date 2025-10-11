2a5764865b13bb18e3d6288267fabb19f5455a20
bumbledor <antoniojardim@hotmail.co.uk>
2025-10-11 09:07:31 +0200

    Setup of benchmark experiment on ants laptop. #44

--- HMM_Forecast/script/benchmark.jl
+++ HMM_Forecast/script/benchmark.jl
@@ -5,13 +5,13 @@ using Revise
 using HMM_Forecast
 
 # @Antonio: Zuerst diese Zeile ausführen um zu testen ob Code grundsätzlich läuft. Wenn es läuft, einfach löschen.
-HMM_Forecast.TEST_trainLinearQR(1)
+# HMM_Forecast.TEST_trainLinearQR(1)
 
 # @Antionio: Dann unteren Code mit Strg+/ auskommentieren und durchlaufen lassen. Rechnet wsl 2 Tage. 
 
-# hhs = 1:5
-# for hh in hhs
-#     HMM_Forecast.trainLinearQR(hh)
-# end
+hhs = 1:5
+for hh in hhs
+    HMM_Forecast.trainLinearQR(hh)
+end
 
-# println("\n ------------------ FINISH ---------------------")
+println("/n ------------------ FINISH ---------------------")
